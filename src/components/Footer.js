78635350df04e609085cc9777e110f6ac9274876
sarandeepsinghghai <157305906+sarandeepsinghghai@users.noreply.github.com>
2024-07-26 01:09:55 +0530

    footer details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/sarandeep-singh-ghai-7439722b3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.instagram.com/array_sahib?igsh=MWNidGM5bnJvbWF3OA%3D%3D&utm_source=qr"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://github.com/sarandeepsinghghai"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Thank You For Giving Your Time.  </p>
           </Col>
