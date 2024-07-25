596df41b514b41f570f72a1f5c2b3d4c596e094e
sarandeepsinghghai <157305906+sarandeepsinghghai@users.noreply.github.com>
2024-07-26 00:22:54 +0530

    restore banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-import logo1 from "../assets/img/logo1.png"; // Import the logo image as logo1
 import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -63,9 +62,9 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Innovating the Future</span>
-                <h1>{`Hi! I'm Sarandeep`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Python Developer", "Database Manager" ]'><span className="wrap">{text}</span></span></h1>
-                <p>Passionate about leveraging technology to drive impactful solutions. Join me on a journey of innovation and excellence.</p>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Sarandeep`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                <p>To succeed in an environment of growth and excellence and earn a job which provides me job satisfaction and self-development and help me achieve personal as well as organizational goals.</p>
                 <button onClick={handleConnectClick}><span>Let's Connect</span> <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
@@ -75,7 +74,6 @@ export const Banner = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
                   <img src={headerImg} alt="Header Img"/>
-                  <img src={logo1} alt="Logo" style={{ marginTop: '20px', maxWidth: '100px' }} /> {/* Add the logo image */}
                 </div>}
             </TrackVisibility>
           </Col>
